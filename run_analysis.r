d1247ac324ab71aaec3a8f48c8aeb0306295520f
whfreelove <whfreelove@gmail.com>
2014-05-20 22:12:42 -0700

    Create run_analysis.r stub

---
+++ run_analysis.r
@@ -0,0 +1,5 @@
+# Merge training and test sets to create one data set
+# Extract only the measurements on the mean and standard deviation for each measurement
+# Uses descriptive activity names to name the activities in the data set
+# Appropriately labels the data set with descriptive activity names
+# Creates a second, independent tidy data set with the average of each variable for each activity and each subject
